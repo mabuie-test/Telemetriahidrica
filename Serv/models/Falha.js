3c07e137ee755a4be6ab34e92a1fce162668fce3
JorgeMabuie <jorgemaabuie@gmail.com>
2025-07-13 11:29:57 +0200

    Create Falha.js

---
+++ Serv/models/Falha.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const FalhaSchema = new mongoose.Schema({
+  medidorId: { type: mongoose.Schema.Types.ObjectId, ref: 'Medidor', required: true },
+  tipo:      { type: String, enum: ['sem_consumo','vazamento','offline'], required: true },
+  inicio:    { type: Date, default: Date.now },
+  fim:       Date,
+  detalhes:  String
+});
+
+module.exports = mongoose.model('Falha', FalhaSchema);
