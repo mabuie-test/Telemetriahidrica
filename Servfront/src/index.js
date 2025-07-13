eccc3d4736874e33939333b3a9b1233be82d58fb
JorgeMabuie <jorgemaabuie@gmail.com>
2025-07-13 15:06:52 +0200

    Update index.js

--- Servfront/src/index.js
+++ Servfront/src/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { AuthProvider } from './context/AuthContext';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import './styles/main.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Router>
+  <HashRouter>
     <AuthProvider>
       <App />
     </AuthProvider>
-  </Router>
+  </HashRouter>
 );
