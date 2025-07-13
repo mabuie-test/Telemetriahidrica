e39c06e3e6fe4662327fe97d7fbaa34e6d887681
JorgeMabuie <jorgemaabuie@gmail.com>
2025-07-13 11:28:51 +0200

    Create Leitura.js

---
+++ Serv/models/Leitura.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const LeituraSchema = new mongoose.Schema({
+  medidorId:     { type: mongoose.Schema.Types.ObjectId, ref: 'Medidor', required: true },
+  consumoDiario: Number,
+  consumoMensal: Number,
+  latitude:      Number,
+  longitude:     Number,
+  bateria:       Number,
+  rssi:          Number,
+  timestamp:     { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Leitura', LeituraSchema);
