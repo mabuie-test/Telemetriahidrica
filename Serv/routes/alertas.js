a6a387755f499b0b028025b5e14807ab74fb339f
JorgeMabuie <jorgemaabuie@gmail.com>
2025-07-13 11:39:32 +0200

    Create alertas.js

---
+++ Serv/routes/alertas.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router  = express.Router();
+const { createAlerta, listAlertas } = require('../controllers/alertasController');
+const { autenticaDispositivo } = require('../middleware/auth');
+
+router.post('/violacao', autenticaDispositivo, createAlerta);
+router.get('/', listAlertas);
+
+module.exports = router;
