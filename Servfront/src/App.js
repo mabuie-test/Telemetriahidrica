7de68a38abaa073a6aeae81b0378eb525dff51f8
JorgeMabuie <jorgemaabuie@gmail.com>
2025-07-13 14:50:18 +0200

    Update App.js

--- Servfront/src/App.js
+++ Servfront/src/App.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+
 
 import { HashRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Navbar from './components/Navbar';
