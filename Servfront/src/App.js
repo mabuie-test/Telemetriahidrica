ebfa2a6c1e77e84d58d54afe34e838af08010b0c
JorgeMabuie <jorgemaabuie@gmail.com>
2025-07-13 15:02:29 +0200

    Update App.js

--- Servfront/src/App.js
+++ Servfront/src/App.js
@@ -1,32 +1,10 @@
+// src/App.js
 import React from 'react';
-import { Routes, Route, Navigate } from 'react-router-dom';
-
-
-import { HashRouter, Routes, Route, Navigate } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Login from './components/Login';
-import PrivateRoute from './components/PrivateRoute';
-import AdminDashboard from './components/AdminDashboard';
-import ClienteDashboard from './components/ClienteDashboard';
 
 export default function App() {
   return (
-    <HashRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/admin" element={
-          <PrivateRoute papel="admin">
-            <AdminDashboard />
-          </PrivateRoute>
-        } />
-        <Route path="/cliente" element={
-          <PrivateRoute papel="cliente">
-            <ClienteDashboard />
-          </PrivateRoute>
-        } />
-        <Route path="*" element={<Navigate to="/login" />} />
-      </Routes>
-    </HashRouter>
+    <div style={{ padding: '2rem', textAlign: 'center' }}>
+      <h1>Olá, esta é uma verificação!</h1>
+    </div>
   );
 }
