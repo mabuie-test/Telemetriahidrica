0085caccf246e1c8f8884bdd9e484151831a197d
JorgeMabuie <jorgemaabuie@gmail.com>
2025-07-13 15:52:24 +0200

    Update auth.js

--- Serv/routes/auth.js
+++ Serv/routes/auth.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router  = express.Router();
 const ctrl    = require('../controllers/authController');
-//const { verificaToken, apenasAdmin } = require('../middleware/auth');
+const { verificaToken, apenasAdmin } = require('../middleware/auth');
 
 router.post('/login',  ctrl.login);
 //router.post('/register', verificaToken, apenasAdmin, ctrl.register);
-//depois comentar e descomentar a linha acima e o cons{ver...
-router.post('/register', ctrl.register);
+depois comentar e descomentar a linha acima e o cons{ver...
+//router.post('/register', ctrl.register);
 module.exports = router;
