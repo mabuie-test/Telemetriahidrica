cbd9c7f9a795c920a37bee50dd352dd80453d9d9
JorgeMabuie <jorgemaabuie@gmail.com>
2025-07-13 09:58:02 +0200

    Create leituras.js

---
+++ Backend/routes/leituras.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router  = express.Router();
+const { createLeitura, listLeituras } = require('../controllers/leiturasController');
+const autenticaDispositivo = require('../middleware/auth');
+
+router.post('/', autenticaDispositivo, createLeitura);
+router.get('/', listLeituras);
+
+module.exports = router;
