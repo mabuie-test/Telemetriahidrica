df3efe2d7df8f06c71a1a9926ca18124f4df8acc
JorgeMabuie <jorgemaabuie@gmail.com>
2025-07-13 16:09:16 +0200

    Update auth.js

--- Serv/routes/auth.js
+++ Serv/routes/auth.js
@@ -4,7 +4,7 @@ const ctrl    = require('../controllers/authController');
 const { verificaToken, apenasAdmin } = require('../middleware/auth');
 
 router.post('/login',  ctrl.login);
-//router.post('/register', verificaToken, apenasAdmin, ctrl.register);
-depois comentar e descomentar a linha acima e o cons{ver...
+router.post('/register', verificaToken, apenasAdmin, ctrl.register);
+//depois comentar e descomentar a linha acima e o cons{ver...
 //router.post('/register', ctrl.register);
 module.exports = router;
