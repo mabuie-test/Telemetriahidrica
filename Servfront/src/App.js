70b2a5d4962bb3eb23ef5fd72cc3d3c094d675f6
JorgeMabuie <jorgemaabuie@gmail.com>
2025-07-15 10:14:01 +0200

    Update App.js

--- Servfront/src/App.js
+++ Servfront/src/App.js
@@ -3,24 +3,28 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Login from './components/Login';
 import PrivateRoute from './components/PrivateRoute';
-import AdminDashboard from './components/AdminDashboard';
-import ClienteDashboard from './components/ClienteDashboard';
 
-import UsersList from './components/UsersList';
-import MedidoresList from './components/MedidoresList';
+import AdminDashboard from './components/AdminDashboard';
+import UsersList      from './components/UsersList';
+import MedidoresList  from './components/MedidoresList';
 import ReadingsIngest from './components/ReadingsIngest';
-import ReadingsView from './components/ReadingsView';
+import ReadingsView   from './components/ReadingsView';
 import FailuresAlerts from './components/FailuresAlerts';
 import ReportsGenerate from './components/ReportsGenerate';
-import AuditLogs from './components/AuditLogs';
+import AuditLogs      from './components/AuditLogs';
+import Accounting     from './components/Accounting';
+
+import ClienteDashboard from './components/ClienteDashboard';
 
 export default function App() {
   return (
     <>
       <Navbar />
       <Routes>
+        {/* Rota de login */}
         <Route path="/login" element={<Login />} />
 
+        {/* Rotas ADMIN */}
         <Route
           path="/admin"
           element={
@@ -78,6 +82,14 @@ export default function App() {
           }
         />
         <Route
+          path="/admin/contabilidade"
+          element={
+            <PrivateRoute papel="admin">
+              <Accounting />
+            </PrivateRoute>
+          }
+        />
+        <Route
           path="/admin/audit"
           element={
             <PrivateRoute papel="admin">
@@ -86,6 +98,7 @@ export default function App() {
           }
         />
 
+        {/* Rotas CLIENTE */}
         <Route
           path="/cliente"
           element={
@@ -94,11 +107,20 @@ export default function App() {
             </PrivateRoute>
           }
         />
+        <Route
+          path="/contabilidade"
+          element={
+            <PrivateRoute papel="cliente">
+              <Accounting />
+            </PrivateRoute>
+          }
+        />
 
+        {/* Qualquer outra rota redireciona para login */}
         <Route path="*" element={<Navigate to="/login" replace />} />
       </Routes>
 
-      {/* Footer copyright */}
+      {/* Footer */}
       <footer className="copyright">
         &copy; 2025 Jorge Mabuie. Todos os direitos reservados.
       </footer>
