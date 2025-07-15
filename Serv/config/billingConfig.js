b64c7f1c6863180ff71b5c5c1fbc70e5b2828c0a
JorgeMabuie <jorgemaabuie@gmail.com>
2025-07-15 09:25:02 +0200

    Create billingConfig.js

---
+++ Serv/config/billingConfig.js
@@ -0,0 +1,5 @@
+module.exports = {
+  consumoMinimo: { x: 10, y: 5000 },    // Até X m³ paga Y (em meticais)
+  extra:          { z: 600 },           // Cada m³ acima de X custa Z
+  multaPercent:   0.10,                 // 10% de mora
+};
