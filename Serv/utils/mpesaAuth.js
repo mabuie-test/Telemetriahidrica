6c60ecf8b818aa941afcba6a902da0baaa4f89a0
JorgeMabuie <jorgemaabuie@gmail.com>
2025-08-26 22:45:42 +0200

    Update mpesaAuth.js

--- Serv/utils/mpesaAuth.js
+++ Serv/utils/mpesaAuth.js
@@ -1,29 +1,14 @@
 // backend/utils/mpesaAuth.js
-const crypto = require('crypto');
-const { apiKey, publicKey } = require('../config/mpesaConfig');
-
-function toPem(base64Key) {
-  // quebra em linhas de 64 chars e adiciona header/footer
-  const chunkSize = 64;
-  const chunks = base64Key.match(new RegExp('.{1,' + chunkSize + '}', 'g')) || [];
-  return `-----BEGIN PUBLIC KEY-----\n${chunks.join('\n')}\n-----END PUBLIC KEY-----`;
-}
-
-function normalizePublicKey(key) {
-  if (!key) throw new Error('MPESA public key não definida');
-  if (key.includes('-----BEGIN PUBLIC KEY-----')) return key;
-  // assume base64 string -> transforma para PEM
-  return toPem(key.replace(/\s+/g, ''));
-}
+const Cryptor = require('./Cryptor');
+const config = require('../config/mpesaConfig');
 
 function generateBearerFromApiKey() {
-  const pem = normalizePublicKey(publicKey);
-  const buffer = Buffer.from(apiKey, 'utf8');
-  const encrypted = crypto.publicEncrypt(
-    { key: pem, padding: crypto.constants.RSA_PKCS1_PADDING },
-    buffer
-  );
-  return encrypted.toString('base64'); // este é o "Authorization: Bearer <aqui>"
+  const apiKey = config.apiKey;
+  const publicKey = config.publicKey;
+  if (!apiKey || !publicKey) {
+    throw new Error('MPESA apiKey ou publicKey não definidos no config.');
+  }
+  return Cryptor.token(publicKey, apiKey);
 }
 
-module.exports = { generateBearerFromApiKey, normalizePublicKey };
+module.exports = { generateBearerFromApiKey };
